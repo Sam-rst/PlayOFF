edd39c80fe0701397f9280b8d891412bf89ca7d4
POWLAIR <114736502+POWLAIR@users.noreply.github.com>
2024-04-19 09:23:19 +0200

    [✨] Add Feature Creation Tournament

---
+++ src/Controller/TournamentController.php
@@ -0,0 +1,52 @@
+<?php
+
+// src/Controller/TournamentController.php
+
+namespace App\Controller;
+
+use App\Entity\Tournament;
+use App\Form\TournamentType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TournamentController extends AbstractController
+{
+    #[Route('/tournament', name: 'app_tournament')]
+    public function index(): Response
+    {
+        return $this->render('tournament/index.html.twig', [
+            'controller_name' => 'TournamentController',
+        ]);
+    }
+
+    #[Route('/tournament/new', name: 'tournament_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $tournament = new Tournament();
+        $form = $this->createForm(TournamentType::class, $tournament);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($tournament);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Tournament created successfully!');
+
+            return $this->redirectToRoute('tournament_success');
+        }
+
+        return $this->render('tournament/new.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+    #[Route('/tournament/success', name: 'tournament_success')]
+    public function success(): Response
+    {
+        return $this->render('tournament/success.html.twig');
+    }
+}
