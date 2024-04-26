803b376fbd7f3ea18754f35c411e556d915b05fd
Comaaa <130038402+Comaaa@users.noreply.github.com>
2024-04-26 11:14:25 +0200

    add statistique + Historique

--- src/Controller/ProfileController.php
+++ src/Controller/ProfileController.php
@@ -2,41 +2,54 @@
 
 namespace App\Controller;
 
-
 use App\Entity\User;
-use App\Repository\UserRepository;
+use App\Repository\TournamentRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProfileController extends AbstractController
 {
     #[Route('/profile', name: 'app_profile')]
-    public function index(): Response
+    public function index(TournamentRepository $tournamentRepository): Response
     {
-
         // Récupérer l'utilisateur connecté
         $user = $this->getUser();
-    
+
         if (!$user instanceof User) {
             throw new \RuntimeException('Utilisateur non trouvé.');
         }
-    
-        // Récupérer les tournois auxquels l'utilisateur a participé
-        $tournamentsParticipated = $user->getTournamentsParticipated();
-    
+
+        // Récupérer les tournois organisés par l'utilisateur
+        $organizerTournaments = $tournamentRepository->findBy(['organisator' => $user]);
 
         return $this->render('profile/index.html.twig', [
             'controller_name' => 'ProfileController',
             'user' => $user,
-            'tournamentsParticipated' => $tournamentsParticipated,
+            'organizerTournaments' => $organizerTournaments,
         ]);
     }
 
     #[Route('/profile/my_tournaments', name: 'my_tournaments')]
     public function myTournaments(): Response
     {
-        return $this->render('tournament/my_tournaments.html.twig');
+        // Récupérer l'utilisateur connecté
+        $user = $this->getUser();
+
+        if (!$user instanceof User) {
+            throw new \RuntimeException('Utilisateur non trouvé.');
+        }
+
+       
+
+        $tournamentsParticipated = $user->getTournamentsParticipated();
+
+        return $this->render('profile/my_tournaments.html.twig', [
+            'user' => $user,
+            'tournamentsParticipated' => $tournamentsParticipated,
+        ]);
+    
     }
+    
 }
- 
+
